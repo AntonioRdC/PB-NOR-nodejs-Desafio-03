97b2ade8d8367945b49c3229c8cb98f749c22e8a
AntonioRdC <antonioribeiroamg@gmail.com>
2022-07-30 15:10:29 -0400

    feat: Create route create Product

---
+++ src/controllers/ProductController.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from 'express'
+
+import Product from '../schemas/Product'
+import Employee from '../schemas/Employee'
+import { ProductValidatorCreate, ProductValidatorUpdate } from '../validator/ProductValidator'
+
+class ProductController {
+  public async store (req: Request, res: Response): Promise<Response> {
+    req.body.price = Number(req.body.price).toFixed(2)
+
+    try {
+      await ProductValidatorCreate.validateAsync({
+        name: req.body.name,
+        category: req.body.category,
+        price: req.body.price,
+        employee_id: req.body.employee_id
+      })
+
+      const employee = await Employee.find({
+        _id: req.body.employee_id,
+        office: 'gerente',
+        situation: 'activate'
+      })
+
+      if (employee.length === 0) {
+        return res.status(400).json({ message: 'Employee invalid' })
+      }
+
+      const product = await Product.create(req.body)
+
+      return res.status(201).json(product)
+    } catch (err) {
+      return res.status(400).json({ message: err.message })
+    }
+  }
+}
+
+export default new ProductController()
